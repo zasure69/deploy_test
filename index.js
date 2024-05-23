c62dddb3f10a1db09260b22f6fa84bc9cf66293b
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-05-23 18:09:42 +0700

    test deploy socket

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const socketio = require('socket.io')
+const http = require('http')
 
+const server = http.createServer(app)
+const io = socketio(server)
+const port = 4000
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
